1971094d75f900d3ae60af9c59d43a36da5b5975
abhishekove <abhishekove@gmail.com>
2019-12-28 02:22:47 +0530

    created basic ui

---
+++ PythonGUI.py
@@ -0,0 +1,60 @@
+from tkinter import *
+
+window = Tk()
+
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l1 = Label(window, text="Author")
+l1.grid(row=0, column=2)
+
+l1 = Label(window, text="Year")
+l1.grid(row=1, column=0)
+
+l1 = Label(window, text="ISBN")
+l1.grid(row=1, column=2)
+
+title_text = StringVar()
+e1 = Entry(window, textvariable=title_text)
+e1.grid(row=0, column=1)
+
+author_text = StringVar()
+e2 = Entry(window, textvariable=author_text)
+e2.grid(row=0, column=3)
+
+year_text = StringVar()
+e3 = Entry(window, textvariable=year_text)
+e3.grid(row=1, column=1)
+
+isbn_text = StringVar()
+e4 = Entry(window, textvariable=isbn_text)
+e4.grid(row=1, column=3)
+
+li = Listbox(window, height=6, width=35)
+li.grid(row=2, column=0,rowspan=6 ,columnspan=2)
+
+sb1 = Scrollbar(window)
+sb1.grid(row=2, column=2, rowspan=6)
+
+li.configure(yscrollcommand=sb1.set)
+sb1.configure(command=li.yview)
+
+b1 = Button(window, text="View all", width=12)
+b1.grid(row=2, column=3)
+
+b1 = Button(window, text="Search Entry", width=12)
+b1.grid(row=3, column=3)
+
+b1 = Button(window, text="Add Entry", width=12)
+b1.grid(row=4, column=3)
+
+b1 = Button(window, text="Update selected", width=12)
+b1.grid(row=5, column=3)
+
+b1 = Button(window, text="Delete selected", width=12)
+b1.grid(row=6, column=3)
+
+b1 = Button(window, text="close", width=12)
+b1.grid(row=7, column=3)
+
+window.mainloop()
